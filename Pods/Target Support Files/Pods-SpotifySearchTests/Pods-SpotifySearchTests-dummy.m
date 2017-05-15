8573ecdbe487fec7fb0d78f8ba288d118eba1c96
Leo Moon <leomoondev@gmail.com>
2017-05-14 23:33:49 -0700

    Add Alamofire to search for songs in Spotify

---
+++ Pods/Target Support Files/Pods-SpotifySearchTests/Pods-SpotifySearchTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpotifySearchTests : NSObject
+@end
+@implementation PodsDummy_Pods_SpotifySearchTests
+@end
--- SpotifySearch/ViewController.swift
+++ SpotifySearch/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class TableViewController: UITableViewController {
+    
+    var names = [String]()
+    
+    var searchURL = "https://api.spotify.com/v1/search?q=Drake&type=track"
+    
+    typealias JSONStandard = [String : AnyObject]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        callAlamo(url: searchURL)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Load some data
+    // Function that grabs some data from the URL
+    func callAlamo(url: String) {
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+        })
     }
-
+    
+    func parseData(JSONData: Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as? JSONStandard
+            if let tracks = readableJSON?["tracks"] as? JSONStandard {
+                if let items = tracks["items"] {
+                    for i in 0..<items.count {
+                        let item = items[i] as! JSONStandard
+                        
+                        let name = item["name"] as! String
+                        names.append(name)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = names[indexPath.row]
+        
+        return cell!
+    }
 
 }
 
